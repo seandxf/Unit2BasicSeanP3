ef050b7e3888e22d0b54da576c8c042474fc7812
Sean Berrios <722320@student.dusd.net>
2024-03-14 11:42:34 -0700

    made the food go
    reguihgri

--- Unit2projectBerriosP3/Assets/Scripts/PlayerController.cs
+++ Unit2projectBerriosP3/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10;
 
+    public Gameobject 
     void Start()
     {
         
@@ -15,11 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position)
+        if(transform.position.x < -xRange)
         {
-
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x > xRange) 
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        }
     }
 }
